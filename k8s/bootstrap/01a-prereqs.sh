86c12dfb03e0db982bdf67156e444e2dd39cc98a
Tim Gibbon <tng@chegwin.org>
2026-08-30 22:10:54 +0100

    Install arch dependency

--- k8s/bootstrap/01a-prereqs.sh
+++ k8s/bootstrap/01a-prereqs.sh
@@ -9,6 +9,13 @@ if [[ -e /etc/debian_version ]]; then
   echo ""
   echo "This looks like a Debian based Linux"
   DEBIAN_VERSION=true
+  if [[ $(uname -m) == "aarch64" ]]
+    then
+       ARCH=arm64
+  elif [[ $(uname -m) == "amd64" ]]
+    then
+       ARCH=amd64
+  fi
 else
   DEBIAN_VERSION=false
 fi  
@@ -40,15 +47,19 @@ elif [[ ${#MISSING[@]} -gt 0 ]] &&  [[ ${DEBIAN_VERSION} == "true" ]]; then
   echo ""
   echo "Install via apt:"
   echo "  sudo apt update"
-  echo "  sudo apt install kind kubectl helm"
-  echo "  curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64"
-  echo "  sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd"
-  echo "  rm argocd-linux-amd64"
+  echo "  sudo apt install kind kubectl"
+  echo "  curl -sSL -o /tmp/argocd-linux-${ARCH} https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-${ARCH}"
+  echo "  sudo install -m 555 /tmp/argocd-linux-${ARCH} /usr/local/bin/argocd"
+  echo "  curl -fsSL -o /tmp/get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4"
+  echo "  chmod 700 /tmp/get_helm.sh"
+  echo "  /tmp/get_helm.sh"
+  echo "  rm /tmp/argocd-linux-${ARCH} /tmp/get_helm.sh"
   echo ""
-  echo "  curl -sSL -o podman.tar.gz https://github.com/podman-container-tools/podman/releases/download/v6.0.2/podman-remote-static-linux_amd64.tar.gz"
-  echo "  tar xvf podman.tar.gz"
-  echo "  cp bin/podman-remote-static-linux_amd64 /usr/local/bin/podman"
-  echo "  rm -rf podman.tar.gz; rm -rf bin/podman-remote-static-linux_amd64"
+  echo "  curl -sSL -o /tmp/podman.tar.gz https://github.com/podman-container-tools/podman/releases/download/v6.0.2/podman-remote-static-linux_${ARCH}.tar.gz"
+  echo "  tar zxvf /tmp/podman.tar.gz"
+  echo "  sudo cp bin/podman-remote-static-linux_${ARCH} /usr/local/bin/podman"
+  echo "  rm -rf bin"
+  echo "  rm -rf /tmp/podman.tar.gz; rm -rf /tmp/bin/podman-remote-static-linux_${ARCH}"
   echo ""
   exit 1
 else 
